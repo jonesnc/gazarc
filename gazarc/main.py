5e0d86e871a039eeb5948eaa1c4e67c136a09dba
Nathan Jones <purkinje90@gmail.com>
2020-12-26 22:21:20 -0700

    feat: use PyInquirer to choose folder name

--- gazarc/main.py
+++ gazarc/main.py
@@ -1,8 +1,11 @@
+import html
 import json
 import os
 import pickle
+import random
 
 import click
+from PyInquirer import prompt
 
 from gazarc import whatapi2
 from gazarc.torrentcheck import (TRACKERS, get_torrent_id, get_torrent_tracker,
@@ -133,6 +136,7 @@ def main(path):
     click.echo(f'running at path: {full_path}')
     for root, dirs, files in os.walk(full_path, topdown=False):
         torrent_dir = os.path.abspath(root)
+        tracker_folder_names = {}
         for name in files:
             if '.torrent' in name:
                 click.echo()
@@ -141,7 +145,7 @@ def main(path):
                 click.echo('Verifying that torrent data is valid...')
                 is_torrent_valid = torrentcheck(torrent_dir, name)
                 if is_torrent_valid:
-                    click.echo('Torrent is valid!')
+                    click.secho(f'{name} is valid!', fg='green')
                     tracker = get_torrent_tracker(torrent_dir, name)
                     torrent_id = get_torrent_id(name)
                     if tracker == 'OPS':
@@ -149,22 +153,9 @@ def main(path):
                     else:
                         torrent = red_handle.request('torrent', id=torrent_id)
 
-                    new_folder_name = get_torrent_folder_name(torrent)
-                    new_folder_absolute = os.path.join(
-                        # parent dir of torrent_dir
-                        os.path.dirname(torrent_dir),
-                        new_folder_name
-                    )
-                    if torrent_dir != new_folder_absolute:
-                        click.echo()
-                        click.echo(f'Renaming folder to: {new_folder_name}')
-                        os.rename(
-                            src=torrent_dir,
-                            dst=new_folder_absolute
-                        )
-                        click.echo(
-                            f'setting torrent_dir to {new_folder_absolute}')
-                        torrent_dir = new_folder_absolute
+                    new_folder_name = html.unescape(
+                        get_torrent_folder_name(torrent))
+                    tracker_folder_names[tracker] = new_folder_name
 
                     torrent_json = json.dumps(
                         torrent, sort_keys=True, indent=4)
@@ -176,8 +167,10 @@ def main(path):
                     click.echo(
                         f'Writing torrent json to: {torrent_json_filename}'
                     )
-                    json_absolute = os.path.join(new_folder_absolute,
-                                                 torrent_json_filename)
+                    json_absolute = os.path.join(
+                        torrent_dir,
+                        torrent_json_filename
+                    )
                     with open(json_absolute, 'w+') as file:
                         file.write(torrent_json)
 
@@ -188,12 +181,61 @@ def main(path):
 
                         os.rename(
                             src=os.path.join(
-                                new_folder_absolute, name),  # .torrent file
+                                torrent_dir, name),  # .torrent file
                             dst=os.path.join(
-                                new_folder_absolute, new_torrent_file_name)
+                                torrent_dir, new_torrent_file_name)
                         )
                 else:
-                    click.echo('Torrent is invalid, exiting!')
+                    click.secho(f'{name} is invalid!', fg='red')
+
+        # no torrent files in a directory
+        if not bool(tracker_folder_names):
+            continue
+
+        # single/multiple torrents with same folder name.
+        if len(set(tracker_folder_names.values())) == 1:
+            new_folder_name = tracker_folder_names[
+                random.choice(list(tracker_folder_names))
+            ]
+        else:
+            # Multiple torrents with different folder names
+            if DEFAULT_TRACKER_FOLDER_NAME in tracker_folder_names:
+                new_folder_name = tracker_folder_names[
+                    DEFAULT_TRACKER_FOLDER_NAME]
+            else:
+                questions = [
+                    {
+                        'type': 'list',
+                        'name': 'tracker',
+                        'message': 'Select your preferred folder name',
+                        'choices': [
+                            {
+                                'key': tracker_name,
+                                'value': folder_name,
+                                'name': f'{tracker_name}: {folder_name}'
+                            }
+                            for tracker_name, folder_name
+                            in tracker_folder_names.items()
+                        ]
+                    }
+                ]
+                answer = prompt(questions)
+                new_folder_name = answer['tracker']
+
+        new_folder_absolute = os.path.join(
+            # parent dir of torrent_dir
+            os.path.dirname(torrent_dir),
+            new_folder_name,
+        )
+
+        if torrent_dir != new_folder_absolute:
+            click.echo()
+            click.echo(f"Renaming folder to: {new_folder_name}")
+            os.rename(src=torrent_dir, dst=new_folder_absolute)
+            click.echo(f"setting torrent_dir to {new_folder_absolute}")
+            torrent_dir = new_folder_absolute
+
+        tracker_folder_names = {}
 
 
 if __name__ == '__main__':
